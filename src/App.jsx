06e05507e41edba435dbc2e2e58f2486ce38d782
Badsha-Nishan <badshanisan14@gmail.com>
2026-03-30 10:46:04 +0600

    added rating section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Banner from "./components/Banner/Banner";
 import Banne from "./components/Banner/Banner";
 import Navbar from "./components/Navbar/Navbar";
+import Rating from "./components/Rating/Rating";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Banner></Banner>
+      <Rating></Rating>
     </>
   );
 }
